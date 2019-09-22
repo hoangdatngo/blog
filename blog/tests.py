c51f5d20e640c6a4b6c6541981dea4cb1c6e694a
Dat <hoangdatngo1@gmail.com>
2019-09-22 11:10:15 +0700

    Add another file

--- blog/tests.py
+++ blog/tests.py
@@ -19,6 +19,9 @@ class BlogTests(TestCase):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
         self.assertEqual(f'{self.post.author}', 'testuser')
@@ -37,3 +40,24 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'),{
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertTemplateUsed(response, 'post_new.html')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Update title',
+            'body': 'Update text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
